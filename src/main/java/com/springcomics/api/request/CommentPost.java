1ebc3432397e3498d43890890aa81a227ae40c6b
spcotoon <springcomicskr@gmail.com>
2025-02-09 05:13:16 +0900

    valid add

--- src/main/java/com/springcomics/api/request/CommentPost.java
+++ src/main/java/com/springcomics/api/request/CommentPost.java
@@ -1,8 +1,10 @@
 package com.springcomics.api.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentPost {
+    @Size(max = 200, message = "200자 이내로 입력해주세요.")
     private final String comment;
 }
